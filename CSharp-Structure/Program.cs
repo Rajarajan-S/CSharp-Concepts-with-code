0a81a2dcd5d7863c866b602a376756462e4da1ea
RAJARAJAN S <rr538855@gmail.com>
2022-11-09 15:57:01 +0530

    Add project files.

---
+++ CSharp-Structure/Program.cs
@@ -0,0 +1,19 @@
+﻿//Namespace - Collection of classes, interfaces, enums etc., Here, 'Console' class in inside the 'System' namespace 
+using System;
+
+class Program // Class
+{
+    static void Main(string[] args)  //Main method - Entry point of your application
+    {
+        Console.WriteLine("Introduction to C#"); // Writes a line
+        Console.WriteLine("\n");
+        Console.WriteLine("Enter your name:");
+        String name = Console.ReadLine(); // Reads a line/input
+        Console.WriteLine("\n");
+        Console.WriteLine("Hai " + name); // Concatenation 
+        Console.WriteLine("Hai {0}", name); // Using placeholder
+        Console.ReadLine();
+    }
+}
+ 
+ 
