e9cb6405bdb61de8fc4dfe62eced52f52ff64512
RAJARAJAN S <rr538855@gmail.com>
2022-11-10 11:02:49 +0530

    ternary operator code

---
+++ Ternary_operator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ternary_operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+
+            bool Isnum5 = num == 10 ? true : false;  
+          //Datatype variable = if Condition (num==10 ?) returns true : else false
+
+            Console.WriteLine("Number = 5 is {0}",Isnum5);
+
+            Console.ReadLine();
+        }
+    }
+}
